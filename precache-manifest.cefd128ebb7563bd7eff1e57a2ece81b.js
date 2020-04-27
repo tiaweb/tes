f177162a64dc6f8696805e07ad8cc0e1f070e618
Yayan He'eh <garanganmanis@yandex.com>
2020-04-27 17:44:14 +0700

    first commit

---
+++ precache-manifest.cefd128ebb7563bd7eff1e57a2ece81b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453bcfb360cacc45bf664601c8db90b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "353e218c9ff84f0b7a62",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "cbe2afaaf6da94ef9211",
+    "url": "/static/css/main.428b4d4f.chunk.css"
+  },
+  {
+    "revision": "353e218c9ff84f0b7a62",
+    "url": "/static/js/2.437ae876.chunk.js"
+  },
+  {
+    "revision": "cbe2afaaf6da94ef9211",
+    "url": "/static/js/main.45940480.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
